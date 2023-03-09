edc7e13d4bf4fe029fff0b110938dc94518a7a4b
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-03-09 00:06:07 -0500

    Created MoreInfo component

---
+++ src/components/MoreInfo/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    width: 90%;
+    flex-direction: column;
+`
+
+export const MoreInfoTitle = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+    color: #f50d41;
+    margin-bottom: 15px;
+`
+
+export const InfoContainer = styled.View`
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+
+    margin-bottom: 15px;
+`
+
+export const Title = styled.Text`
+    color: white;
+    font-weight: bold;
+`
+
+export const  Result = styled.Text`
+    margin-top: 5px;
+    color: white;
+`
