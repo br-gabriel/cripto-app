69d52cb165997253f6ed7a560f4a42481d547a49
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-03-21 21:57:29 -0500

    added routes

--- App.js
+++ App.js
@@ -1,27 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, View } from 'react-native';
-import { CurrentPrice } from './src/components/CurrentPrice/index.js'
-import { HistoryChart } from './src/components/HistoryChart/index.js';
-import { MoreInfo } from './src/components/MoreInfo/index.js';
-import { QuotationList } from './src/components/QuotationList/index.js';
+import { Routes } from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" backgroundColor='#f50d41' />
-      <CurrentPrice />
-      <HistoryChart />
-      <QuotationList />
-      <MoreInfo></MoreInfo>
-    </View>
+    <>
+      <Routes />
+      <StatusBar style="light" translucent />
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    paddingTop: Platform.OS === 'android' ? 60 : 0,
-  },
-});
