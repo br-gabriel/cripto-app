55572bed82228e30ade2642d2541e244f25a0df9
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-03-22 00:10:33 -0500

    Font Inter added

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { Routes } from './src/routes';
+import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter'
+import { Text } from 'react-native';
 
 export default function App() {
+  const [fontsLoader] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold
+  });
+
+  if(!fontsLoader) {
+    return (
+      <Text>Carregando...</Text>
+    )
+  }
+
   return (
     <>
       <Routes />
