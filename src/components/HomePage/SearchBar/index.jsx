6852d2eb17ffd6cdb4f8dd3df6582b9cc79ada49
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-03-21 23:26:12 -0500

    added search bar

--- src/components/HomePage/SearchBar/index.jsx
+++ src/components/HomePage/SearchBar/index.jsx
@@ -1,7 +1,13 @@
-import { Search } from "./styles";
+import { SearchContainer, SearchField, SearchButton } from "./styles";
+import { Ionicons } from '@expo/vector-icons';
 
 export function SearchBar() {
     return (
-        <Search />
+        <SearchContainer>
+            <SearchField />
+            <SearchButton>
+                <Ionicons name="search" size={24} color="white" />
+            </SearchButton>
+        </SearchContainer>
     )
 }
--- src/components/HomePage/SearchBar/styles.js
+++ src/components/HomePage/SearchBar/styles.js
@@ -1,11 +1,33 @@
 import styled from "styled-components/native";
 
-export const Search = styled.TextInput`
+export const SearchContainer = styled.View`
     width: 90%;
     height: 45px;
     margin-top: 26px;
+    flex-direction: row;
+    gap: 0;
+`
+
+export const SearchField = styled.TextInput`
+    width: 85%;
+    height: 45px;
     padding-left: 20px;
-    border-radius: 50px;
+    border-top-left-radius: 50px;
+    border-bottom-left-radius: 50px;
     color: white;
+    background-color: #11104D;
+`
+
+export const SearchButton = styled.TouchableOpacity.attrs({
+    activeOpacity: 1
+})`
+    height: 45px;
+    width: 15%;
+    border-top-right-radius: 50px;
+    border-bottom-right-radius: 50px; 
+    
+    align-items: center;
+    justify-content: center;
+    
     background-color: #11104D;
 `
