1a84b422f2fbaf24a1c8eca3e0a28305c15513bd
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-04-10 17:32:27 -0500

    Added main coin info

---
+++ src/components/DetailsPage/CurrentPrice/styles.js
@@ -0,0 +1,27 @@
+import { Platform } from "react-native";
+import styled from "styled-components/native";
+
+export const HeaderPrice = styled.View`
+    width: 100%;
+    height: auto;
+    align-items: flex-start;
+    padding: 20px;
+    margin-bottom: 20px;
+`
+
+export const CriptoName = styled.Text`
+    color: white;
+    font-weight: bold;
+    font-size: 16px;
+`
+
+export const Price = styled.Text`
+    color: white;
+    font-size: 28px;
+    font-weight: bold;
+    margin-top: 11px;
+`
+
+export const PriceLabel = styled.Text`
+    color: white;
+`
