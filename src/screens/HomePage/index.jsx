8039926aaf0a98ead222f3ae843501ff53f77831
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-04-10 17:28:48 -0500

    fetch data and loading component added

--- src/screens/HomePage/index.jsx
+++ src/screens/HomePage/index.jsx
@@ -1,14 +1,45 @@
+import { Alert } from 'react-native';
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
 import { Header } from '../../components/HomePage/AppLogo';
 import { CriptoList } from '../../components/HomePage/CriptosList';
 import { SearchBar } from '../../components/HomePage/SearchBar';
 import { Container } from './styles';
+import { Loading } from '../../components/index'
 
 export function Home() {
+    const [loading, setLoading] = useState(false)
+    const [currency, setCurrency] = useState('usd')
+    const [data, setData] = useState([]);
+
+    async function fetchData() {
+        try {
+            setLoading(true)
+            const response = await api.get(`coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`)
+            setData(response.data)
+        } catch (error) {
+            Alert.alert('Ops', 'Não foi possível carregar a lista de criptomoedas')
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+    
+    if (loading) {
+        return (
+            <Loading />
+        );
+    };
+
     return (
         <Container>
             <Header />
             <SearchBar />
-            <CriptoList />
+            <CriptoList data={data} currency={currency} setCurrency={setCurrency}/>
         </Container>
     )
 };
