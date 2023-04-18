16c547b57aff09aaffa0187138b8871b114f7216
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-04-17 21:36:21 -0500

    detail page styles

--- src/screens/Details/styles.js
+++ src/screens/Details/styles.js
@@ -1,1 +1,34 @@
 import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+    flex: 1;
+    align-items: center;
+    background-color: #05043D;
+    padding-top: 32px;
+`
+
+export const Header = styled.View`
+    width: 90%;
+    align-items: flex-start;
+    justify-content: center;
+
+    margin-top: 16px;
+    gap: 5px;
+`
+
+export const Title = styled.Text`
+    font-size: 16px;
+    color: white;
+    font-weight: bold;
+`
+
+export const Price = styled.Text`
+    font-size: 28px;
+    color: white;
+    font-weight: bold;
+`
+
+export const Percentage = styled.Text`
+    font-size: 16px;
+    color: ${(props) => (props.value < 0 ? '#FF3030' : '#0CFF6D')};
+`
