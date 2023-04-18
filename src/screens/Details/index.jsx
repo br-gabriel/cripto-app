b34726d8b5f4f24e63eed85ed602ab8897966f5e
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-04-17 21:36:37 -0500

    detail page created

--- src/screens/Details/index.jsx
+++ src/screens/Details/index.jsx
@@ -1,5 +1,22 @@
-export function Details() {
+import { Container, Title, Price, Percentage, Header } from "./styles";
+import { HistoryChart } from "../../components/DetailsPage/HistoryChart";
+import { ChartFilter } from "../../components/DetailsPage/ChartFilter"; 
+
+export function Details({ route }) {
+    const { item, currency } = route.params;
+    
     return (
-        <></>
+        <Container>
+            <Header>
+                <Title>{item.name}</Title>
+                <Price>$ {item.current_price.toFixed(2)}</Price>
+                <Percentage value={item.price_change_percentage_24h}>
+                    {item.price_change_percentage_24h.toFixed(2)}%
+                </Percentage>
+            </Header>
+
+            <HistoryChart />
+            <ChartFilter />
+        </Container>
     )
 };
