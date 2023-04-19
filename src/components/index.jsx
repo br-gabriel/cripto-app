441e8da588600b75550d3cf9e21aa0a8cbe543b6
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-04-19 16:57:21 -0500

    Loading component has been renamed

--- src/components/index.jsx
+++
@@ -1,9 +0,0 @@
-import { View, ActivityIndicator } from "react-native";
-
-export function Loading() {
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#05043D'}}>
-            <ActivityIndicator color='#fff'/>
-        </View>
-    )
-}
