e32ea1f88ff00d9c114eca6ce2ecee8b816f486b
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-04-19 17:24:16 -0500

    Added loading propertie

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,8 @@
 import { View, ActivityIndicator } from "react-native";
 
-export function Loading() {
+export function Loading({bgColor}) {
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#05043D'}}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: bgColor}}>
             <ActivityIndicator color='#fff'/>
         </View>
     )
