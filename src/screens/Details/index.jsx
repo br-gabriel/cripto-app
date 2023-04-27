7711b80e5a231890673f84cce9c0a7b937a98b48
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-04-26 21:11:13 -0500

    Added formatting function and filter value

--- src/screens/Details/index.jsx
+++ src/screens/Details/index.jsx
@@ -2,24 +2,37 @@ import { Container, Title, Price, Percentage, Header } from "./styles";
 import { HistoryChart } from "../../components/DetailsPage/HistoryChart";
 import { ChartFilter } from "../../components/DetailsPage/ChartFilter"; 
 import { BackButton } from "../../components/BackButton";
+import { useState } from "react";
 
 export function Details({ route }) {
     const { item, currency } = route.params;
-    
+    const [filter, setFilter] = useState(15);
+
+    function currencyFormatter(currency, price) {
+        const formatter = new Intl.NumberFormat(currency == 'usd' ? 'en-US' : 'pt-BR', {
+            style: 'currency',
+            currency: currency.toUpperCase(),
+        })
+
+        return formatter.format(price);
+    }
+
     return (
         <Container>
             <BackButton />
             <Header>
                 <Title>{item.name}</Title>
-                <Price>$ {item.current_price.toFixed(2)}</Price>
+                <Price>
+                    {currencyFormatter(currency, item.current_price)}
+                </Price>
                 <Percentage value={item.price_change_percentage_24h}>
                     {item.price_change_percentage_24h > 0 ? '+' : ''}
                     {item.price_change_percentage_24h.toFixed(2)}%
                 </Percentage>
             </Header>
 
-            <HistoryChart name={item.name} currency={currency} days={15}/>
-            <ChartFilter />
+            <HistoryChart name={item.id} currency={currency} days={filter}/>
+            <ChartFilter filter={filter} setFilter={setFilter}/>
         </Container>
     )
 };
