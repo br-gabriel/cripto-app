71b5104edc48ec53937e35cb9f20b441d9b2c3a8
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-04-28 16:16:11 -0500

    Added filter operation

--- src/components/DetailsPage/ChartFilter/index.js
+++ src/components/DetailsPage/ChartFilter/index.js
@@ -1,31 +1,41 @@
-import { Filter, FilterButton, FilterText  } from "./styles";
+import { Filter, FilterButton, FilterText } from "./styles";
 
-export function ChartFilter() {
+export function ChartFilter({ setFilter, setInterval }) {
+    function handleFilter(value) {
+        if (value === "1") {
+            setInterval('')
+            setFilter(value)
+        } else {
+            setInterval('&interval=daily')
+            setFilter(value)
+        }
+    }
+
     return (
         <Filter>
-            <FilterButton>
-                <FilterText>H</FilterText>
-            </FilterButton>
-
-            <FilterButton>
-                <FilterText>D</FilterText>
-            </FilterButton>
-
-            <FilterButton>
+            <FilterButton onPress={() => handleFilter("7")}>
                 <FilterText>7D</FilterText>
             </FilterButton>
 
-            <FilterButton>
-                <FilterText>M</FilterText>
+            <FilterButton onPress={() => handleFilter("15")}>
+                <FilterText>15D</FilterText>
             </FilterButton>
 
-            <FilterButton>
+            <FilterButton onPress={() => handleFilter("31")}>
+                <FilterText>1M</FilterText>
+            </FilterButton>
+
+            <FilterButton onPress={() => handleFilter("180")}>
+                <FilterText>6M</FilterText>
+            </FilterButton>
+
+            <FilterButton onPress={() => handleFilter("365")}>
                 <FilterText>1A</FilterText>
             </FilterButton>
 
-            <FilterButton>
+            {/* <FilterButton onPress={() => handleFilter("max")}>
                 <FilterText>MAX</FilterText>
-            </FilterButton>
+            </FilterButton> */}
         </Filter>
     )
 }
