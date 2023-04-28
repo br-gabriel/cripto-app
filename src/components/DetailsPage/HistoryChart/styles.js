8aea0f2ec6197e1af2585f0dccaffe44313ab445
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-04-28 16:46:51 -0500

    style update

--- src/components/DetailsPage/HistoryChart/styles.js
+++ src/components/DetailsPage/HistoryChart/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 export const ChartContainer = styled.View`
     background-color: #272657;
     height: 305px;
-    width: 92%;
+    width: 100%;
     border-radius: 5px;
     
     margin-top: 20px;
