063025d7fa7e15010fe990d11e827cc4857ae00f
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-04-28 16:24:51 -0500

    Loading rendering change

--- src/components/DetailsPage/HistoryChart/index.jsx
+++ src/components/DetailsPage/HistoryChart/index.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
-import { Alert, Dimensions, Text } from 'react-native';
+import { Alert, Dimensions } from 'react-native';
 import { api } from '../../../services/api';
 import { LineChart } from "react-native-chart-kit";
 import { Loading } from '../../Loading';
 import { format } from "date-fns";
 import { ChartContainer } from "./styles";
 
-export function HistoryChart({name, currency, days}) {
-    const [loading, setLoading] = useState(false);
+export function HistoryChart({ name, currency, days, interval }) {
+    const [loading, setLoading] = useState(true);
     const [chartData, setChartData] = useState([]);
 
     async function fetchData() {
         try {
             setLoading(true)
-            const response = await api.get(`coins/${name}/market_chart?vs_currency=${currency}&days=${days-1}&interval=daily`)
+            const response = await api.get(`coins/${name}/market_chart?vs_currency=${currency}&days=${days - 1}${interval}`)
 
             const formattedData = await response.data.prices.map(price => {
                 const formattedDate = format(new Date(price[0]), 'yyyy-MM-dd');
@@ -31,45 +31,44 @@ export function HistoryChart({name, currency, days}) {
 
     useEffect(() => {
         fetchData()
-    }, [])
+    }, [, days, interval])
 
     return (
         <ChartContainer>
-            {chartData.length > 0 ? (
-                <LineChart
-                data={{
-                    datasets: [{ data: chartData.map((item) => item.price ) }]
-                }}
-
-                width={Dimensions.get('window').width - 32}
-                height={301}
-                yAxisLabel="$"
-                // yAxisSuffix="k"
-                withVerticalLines={false}
-                yLabelsOffset={10}
-                withVerticalLabels={false}
-
-                chartConfig={{
-                    backgroundColor: '#fff',
-                    backgroundGradientFrom: '#272657',
-                    backgroundGradientTo: '#272657',
-                    decimalPlaces: 0,
-                    color: (opacity = 255) => `rgba(255, 255, 255, ${opacity})`,
-                    propsForDots: {
-                        r: '2',
-                        strokeWidth: '1',
-                        stroke: '#fff'
-                    }
-                }}
-                bezier
-                style={{
-                    marginTop: -40,
-                    paddingTop: 40,
-                    borderRadius: 5,
-                }}
-            />
-            ) : (
+            {loading ? (
                 <Loading bgColor={'#272657'} />
+            ) : (
+                <LineChart
+                    data={{
+                        datasets: [{ data: chartData.map((item) => item.price) }]
+                    }}
+
+                    width={Dimensions.get('window').width - 32}
+                    height={301}
+                    yAxisLabel="$"
+                    withVerticalLines={false}
+                    yLabelsOffset={10}
+                    withVerticalLabels={false}
+
+                    chartConfig={{
+                        backgroundColor: '#fff',
+                        backgroundGradientFrom: '#272657',
+                        backgroundGradientTo: '#272657',
+                        decimalPlaces: 0,
+                        color: (opacity = 255) => `rgba(255, 255, 255, ${opacity})`,
+                        propsForDots: {
+                            r: '0',
+                            strokeWidth: '0',
+                            stroke: '#fff'
+                        }
+                    }}
+                    bezier
+                    style={{
+                        marginTop: -40,
+                        paddingTop: 40,
+                        borderRadius: 5,
+                    }}
+                />
             )}
         </ChartContainer>
     )
