14aba03845dd67469548dc17818acdddb3adf54f
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-05-06 00:17:36 -0500

    added search bar function

--- src/components/HomePage/SearchBar/index.jsx
+++ src/components/HomePage/SearchBar/index.jsx
@@ -1,11 +1,24 @@
 import { SearchContainer, SearchField, SearchButton } from "./styles";
 import { Ionicons } from '@expo/vector-icons';
 
-export function SearchBar() {
+export function SearchBar({search, setSearch, searchResult}) {
+    function handleSearchText(text) {
+        setSearch(text)
+    }
+
+    function handleSubmit() {
+        searchResult()
+    }
+    
     return (
         <SearchContainer>
-            <SearchField />
-            <SearchButton>
+            <SearchField 
+                onChangeText={handleSearchText}
+                onSubmitEditing={handleSubmit}
+                value={search}
+            />
+
+            <SearchButton onPress={handleSubmit}>
                 <Ionicons name="search" size={20} color="white" />
             </SearchButton>
         </SearchContainer>
